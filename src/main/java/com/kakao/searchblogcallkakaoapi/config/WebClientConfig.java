b076b2667aa632a290394467c8d4383b9adda533
jaeheonN <njh@foodinko.com>
2023-03-24 12:39:25 +0900

    webClient config

---
+++ src/main/java/com/kakao/searchblogcallkakaoapi/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package com.kakao.searchblogcallkakaoapi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@Slf4j
+public class WebClientConfig {
+
+    @Value("${kakao.baseUri}")
+    String kakaoBaseUri;
+    @Bean(name="kakaoWebClient")
+    public WebClient webClient(){
+        return WebClient.builder()
+                .baseUrl(this.kakaoBaseUri)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build()
+                .mutate()
+                .build();
+    }
+}
--- src/main/java/com/kakao/searchblogcallkakaoapi/serviceImpl/BlogSearchServiceImpl_kakao_001.java
+++ src/main/java/com/kakao/searchblogcallkakaoapi/serviceImpl/BlogSearchServiceImpl_kakao_001.java
@@ -5,10 +5,12 @@ import com.kakao.searchblogcallkakaoapi.service.BlogSearchService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -20,22 +22,13 @@ public class BlogSearchServiceImpl_kakao_001 implements BlogSearchService {
     @Value("${kakao.token}")
     private String authorization;
 
-    private WebClient webClient;
-
-    @PostConstruct
-    public void initWebClient() {
-        webClient = WebClient.builder()
-                .baseUrl("https://dapi.kakao.com")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .build()
-                .mutate()
-                .build();
-    }
+    @Qualifier("kakaoWebClient")
+    private final WebClient webClient;
 
     @Override
     public Mono<KakaoResponse> getBlogsFromApi(String query, String sort, int page, int size){
-        return webClient.get().uri(uriBuilder1 ->
-                        uriBuilder1.path("/v2/search/blog")
+        return webClient.get().uri(uriBuilder ->
+                        uriBuilder.path("/v2/search/blog")
                                 .queryParam("query", query)
                                 .queryParam("sort", sort)
                                 .queryParam("page", page)
